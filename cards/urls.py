2c8c8cce45e497563fec389c1736a1d2feb638f9
Chekwube Utomi <chekwubeutomi@gmail.com>
2024-08-19 00:22:31 +0100

    updateview added

--- cards/urls.py
+++ cards/urls.py
@@ -8,5 +8,15 @@ urlpatterns = [
         views.CardListView.as_view(),
         name="card-list"
     ),
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+        ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    )
 
 ]
